89685f1afe31bb6a4cb1bfbf6debac3c969c26dd
Mariam Assem <maryamassem11@gmail.com>
2025-11-24 19:08:42 +0200

    Phase 1: Initial setup and project structure

---
+++ Notification_Service/app.py
@@ -0,0 +1,11 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Order Service is Running!"
+
+if __name__ == '__main__':
+    # انتبهي لرقم البورت المخصص لكل خدمة كما في المستند
+    app.run(port=5005, debug=True)
